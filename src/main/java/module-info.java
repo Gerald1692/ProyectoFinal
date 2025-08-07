a739112b484468469993f3752cf7e98d49bca64d
Marcela Abarca Delgado <admarcela5@gmail.com>
2025-08-07 15:02:38 -0600

    Se conecto la base de datos y se actualizo el javafx
    Se conecto la base de datos y se actualizo el javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.mycompany.proyectofinal {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.base;
+    requires java.sql;
+    
 
-    opens com.mycompany.proyectofinal to javafx.fxml;
+    opens com.mycompany.proyectofinal to javafx.fxml, javafx.base;
+ 
     exports com.mycompany.proyectofinal;
 }
