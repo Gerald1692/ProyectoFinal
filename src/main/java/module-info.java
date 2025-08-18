6e44e664ee6fde09b1b59eb2abc0910b0405723a
Marcela Abarca Delgado <admarcela5@gmail.com>
2025-08-17 22:42:24 -0600

    Reapply "obra"
    This reverts commit c5e58c3ad0c228f93d49e52302da2eab0d7f4b57.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.mycompany.proyectofinal {
+    // Módulos de JavaFX
     requires javafx.controls;
     requires javafx.fxml;
+<<<<<<< Updated upstream
     requires java.sql;
     requires jbcrypt;
     requires java.desktop;
@@ -19,3 +21,16 @@ module com.mycompany.proyectofinal {
     exports com.mycompany.proyectofinal.Controllers;
     requires javafx.mediaEmpty;
 }
+=======
+    
+    
+    requires java.sql;      
+    requires jbcrypt;       
+    
+    // Abre tu paquete principal a javafx.fxml
+    opens com.mycompany.proyectofinal to javafx.fxml;
+    
+    // Exporta tu paquete principal
+    exports com.mycompany.proyectofinal;
+}
+>>>>>>> Stashed changes
